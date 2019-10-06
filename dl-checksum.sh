7555765436de153860c25d392a77db440dac5d94
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-06 15:08:55 -0500

    up to v2.1.0. nominal Darwin/Windows support.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env sh
-VER=v2.0.0
+VER=${1:-v2.1.0}
 DIR=~/Downloads
 MIRROR=https://github.com/nats-io/nats-server/releases/download/${VER}
 
 dl()
 {
-    OS=$1
-    PLATFORM=$2
-    FILE=nats-server-${VER}-${OS}-${PLATFORM}.zip
-    URL=$MIRROR/$FILE
-    wget --quiet -O $DIR/$FILE $URL
-    printf "  # %s\n  %s-%s: sha256:%s\n" $URL $OS $PLATFORM `sha256sum $DIR/$FILE | awk '{print $1}'`
+    local os=$1
+    local arch=$2
+    local platform="${os}-${arch}"
+    local file=nats-server-${VER}-${platform}.zip
+    local lfile=$DIR/$file
+    local url=$MIRROR/$file
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
 }
 
-printf "%s:\n" $VER
+printf "  %s:\n" $VER
 dl darwin amd64
 dl linux 386
 dl linux amd64
